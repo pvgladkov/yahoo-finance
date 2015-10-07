6514a44642f9e834cbf5c2f23b1eba2d9ca02a8a
Paul Gladkov <webgeist.xor@gmail.com>
2015-10-07 09:45:25 +0300

    init

--- src/main/scala/cc/p2k/yahoo/finance/Stock.scala
+++ src/main/scala/cc/p2k/yahoo/finance/Stock.scala
@@ -24,30 +24,20 @@ object Stock {
 
   def apply(symbol: String) : Stock = {
     val sym = symbol.toUpperCase
-    var price = 0.0
-    var volume = 0
-    var pe = 0.0
-    var eps = 0.0
-    var week52low = 0.0
-    var week52high = 0.0
-    var dayLow = 0.0
-    var dayHigh = 0.0
-    var movingAv50day = 0.0
-    var marketCap = 0.0
 
     val s = Source.fromURL("http://finance.yahoo.com/d/quotes.csv?s="+ symbol + "&f=l1vr2ejkghm3j3").mkString
     val info = s.split(",")
 
-    price = getDouble(info(0))
-    volume = getInt(info(1))
-    pe = getDouble(info(2))
-    eps = getDouble(info(3))
-    week52low = getDouble(info(4))
-    week52high = getDouble(info(5))
-    dayLow = getDouble(info(6))
-    dayHigh = getDouble(info(7))
-    movingAv50day = getDouble(info(8))
-    marketCap = getDouble(info(9))
+    val price = getDouble(info(0))
+    val volume = getInt(info(1))
+    val pe = getDouble(info(2))
+    val eps = getDouble(info(3))
+    val week52low = getDouble(info(4))
+    val week52high = getDouble(info(5))
+    val dayLow = getDouble(info(6))
+    val dayHigh = getDouble(info(7))
+    val movingAv50day = getDouble(info(8))
+    val marketCap = getDouble(info(9))
 
     new Stock(sym, price, volume, pe, eps, week52low, week52high, dayLow, dayHigh, movingAv50day, marketCap)
   }
